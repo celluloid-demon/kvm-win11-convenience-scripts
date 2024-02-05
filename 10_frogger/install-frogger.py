0e250878f66608e6feb8f87d664c60e92730f15c
jonathan <celluloid-demon@users.noreply.github.com>
2024-02-04 16:17:03 -0800

    tweaks

--- 10_frogger/install-frogger.py
+++ 10_frogger/install-frogger.py
@@ -1,33 +1,15 @@
-import requests
+from urllib.request import urlretrieve
 
-URL = "https://archive.org/download/froggerusarerelease19980310.7z/Frogger%20%28USA%29%20%28Rerelease%29%20%2819980310%29.7z"
+OUT = "frogger.7z"
+URL = (
 
-def get_archive(url, destination="downloaded_file.7z"):
-
-	try:
-
-		response = requests.get(url)
-
-		if response.status_code == 200:
-
-			with open(destination, 'wb') as file:
-
-				file.write(response.content)
-
-			print(f"Download successful. File saved as '{destination}'")
-
-		else:
-
-			print(f"Failed to download. Status code: {response.status_code}")
-
-	except Exception as e:
-
-		print(f"An error occurred: {e}")
+		"https://archive.org/download/froggerusarerelease19980310.7z/"
+		"Frogger%20%28USA%29%20%28Rerelease%29%20%2819980310%29.7z"
+	
+	)
 
 def main():
 
-	get_archive(URL)
+	urlretrieve(URL, OUT)
 
-if __name__ == "__main__":
-
-	main()
+main()
